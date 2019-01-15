ed6963a36b6ee97feedcc15ed6d94cac25a26a14
jas7o <jasio2k00@gmail.com>
2019-01-15 18:26:58 +0000

    initial commit #3

---
+++ compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+export ARCH=arm
+export SUBARCH=arm
+
+ccache -M 10G
+export USE_CCACHE=1
+export CCACHE_DIR=/home/jas7o/.ccache
+
+export TOP=/home/jas7o/android
+export PATH=$TOP/toolschain/arm-cortex_a8-linux-gnueabi-linaro_4.8.3-2013.12/bin/:$PATH
+
+export CROSS_COMPILE=arm-cortex_a8-linux-gnueabi-
+
+make a31ul_defconfig
+#make menuconfig
+make arch=arm
---
+++ dtb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear
+
+./mkbootimg_tools/dtbTool -o dt.img ../arch/arm/boot/dts
