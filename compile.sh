deca17d1b4555fce40c9765b2f9a02647d79c488
jas7o <jasio2k00@gmail.com>
2019-01-18 20:01:10 +0000

    sh

--- compile.sh
+++ compile.sh
@@ -2,19 +2,18 @@
 
 clear
 
-#export ARCH=arm
-#export SUBARCH=arm
+export ARCH=arm
+export SUBARCH=arm
 
-#ccache -M 10G
-#export USE_CCACHE=1
-#export CCACHE_DIR=/home/jas7o/.ccache
+ccache -M 10G
+export USE_CCACHE=1
+export CCACHE_DIR=/home/jas7o/.ccache
 
-#export TOP=/home/jas7o/android
-#export PATH=$TOP/toolschain/arm-cortex_a8-linux-gnueabi-linaro_4.8.3-2013.12/bin/:$PATH
+export TOP=/home/jas7o/android
+export PATH=$TOP/toolschain/arm-cortex_a8-linux-gnueabi-linaro_4.8.3-2013.12/bin/:$PATH
 
-#export CROSS_COMPILE=arm-cortex_a8-linux-gnueabi-
+export CROSS_COMPILE=arm-cortex_a8-linux-gnueabi-
 
-make a31ul-jas7o_defconfig
-#make .config
-
+#make a31ul-jas7o_defconfig
+make .config
 make menuconfig
