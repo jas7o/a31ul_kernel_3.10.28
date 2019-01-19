a2ca3105793d2a25faeb6098c31d4b47e06a92d0
jas7o <jasio2k00@gmail.com>
2019-01-19 15:22:42 +0000

    sh

--- dtb_boot.sh
+++ dtb_boot.sh
@@ -7,4 +7,9 @@ cp ./arch/arm/boot/zImage ./boot_out
 
 find ./ -name "*.ko" -exec cp {} ./boot_out/modules/ \;
 
+arm-cortex_a8-linux-gnueabi-strip -g ./boot_out/modules/wlan.ko
+
 ./mkbootimg_tools/dtbToolCM -s 2048 -d "htc,project-id = <" -o ./boot_out/dt.img -p ./scripts/dtc/ ./arch/arm/boot/
+
+
+
