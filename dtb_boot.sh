7f9a2e6224545afc6b4760e8299d406334887370
jas7o <jasio2k00@gmail.com>
2019-01-19 14:47:19 +0000

    cfg

--- dtb_boot.sh
+++ dtb_boot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-rm dt.img
+rm ./boot_out/dt.img
+rm ./boot_out/zImage
 
 cp ./arch/arm/boot/zImage ./boot_out
-./mkbootimg_tools/dtbTool -p ./scripts/dtc/ -o ./boot_out/dt.img ./arch/arm/boot/dts/
+
+find ./ -name "*.ko" -exec cp {} ./boot_out/modules/ \;
+
+./mkbootimg_tools/dtbToolCM -s 2048 -d "htc,project-id = <" -o ./boot_out/dt.img -p ./scripts/dtc/ ./arch/arm/boot/
